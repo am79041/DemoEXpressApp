9b774674acffcfafaf916390afb226a5d869f805
Kumar <am79041.ak@gmail.com>
2023-01-30 14:09:48 +0530

    cron removed

--- app.ts
+++ app.ts
@@ -1,16 +1,10 @@
 import express from "express";
-import { CronJob } from "cron";
 import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const job = new CronJob("*/5 * * * * *", () => {
-  console.log("Give me Blowjob");
-});
-
 app.get("/", (req, res) => {
-  job.start();
   res.status(200).json({ msg: "success" });
 });
 
