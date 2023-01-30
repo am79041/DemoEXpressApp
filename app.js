9e8c31a046d96490ed6384568a31f81b16fb9249
Kumar <am79041.ak@gmail.com>
2023-01-30 14:26:59 +0530

    javascript all here

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-require("dotenv/config");
-const app = (0, express_1.default)();
+import express from "express";
+import "dotenv/config";
+
+const app = express();
 const port = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
-    res.status(200).json({ msg: "success" });
+  res.status(200).json({ msg: "success" });
 });
+
 app.listen(port, () => console.log(`Server is running at PORT: ${port}`));
